078a38f9bdee74a822d774041e0b14b7cd9f4193
james <jag0007@auburn.edu>
2023-02-20 16:51:57 -0600

    create conv test.cpp

---
+++ conv/test/src/test.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+
+int main(argc, char** argv) {
+  
+  Mat img = imread("greyGoat.jpeg", IMREAD_COLOR) // this probably needs to change
+  
+  imshow("Goat!", img);
+
+  int red = 0;
+  int green = 0;
+  int blue = 0;
+  Mat greyMat(img.rows, img.cols, CV_8UC1, Scalar(0));
+  for (int rowIdx = 0; rowIdx < img.rows; ++rowIdx) {
+    for (int colIdx = 0; colIdx < img.cols; ++colIdx) {
+      auto & vec = img.at<cv::Vec<uchar, 3>>(rowIdx, colIdx);
+      blue = vec[0];
+      green = vec[1];
+      red = vec[2];
+      greyMat.at<uchar>(rowIdx, colIdx) = 
+        red * 3.0 / 10.0 + green * 6.0 / 10.0 + blue * 1.0 / 10.0;
+    }
+  }
+
+  imshow("Grayscale goat!", greyMat);
+  imwrite("Grayscalegoat.jpg", greyMay);
+  
+  waitKey(0);
+
+  return 0;
+}
