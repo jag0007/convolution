b8989e5b62f1e07f6fbb5d7577436140b6e4c863
james <jag0007@auburn.edu>
2023-02-20 17:53:30 -0600

    Add Makefile for project

---
+++ Makefile
@@ -0,0 +1,36 @@
+CC=gcc
+CXX=g++
+NVCC=nvcc
+
+CUDAPATH=/opt/asn/apps/cuda_11.7.0
+
+CCFLAGS=-std=c11
+CXXFLAGS=-std=c++11 -04
+NVCCFLAGS=-std=c++11
+
+NVCCARCHS=-gencode arch=compute_80,code=sm_80 -gencode arch=compute_70,code=sm_70
+
+TIMERINCPATH=-I$(CUDAPATH)/include -ITimer/include
+INCPATH=-Isaxpy/include -I$(CUDAPATH)/include -I$(CUDAPATH)/samples/common/inc -I$(GTESTPATH)/include
+LIBPATH=-L$(CUDAPATH)/lib64 -L$(GTESTPATH)/lib64
+RPATH=-Wl,-rpath=`pwd`/build/lib -Wl,-rpath=`pwd`/$(GTESTPATH)/lib64 -Wl,-rpath=`pwd`/$(CUDAPATH)/lib64
+LIBS=-lcudart
+
+.PHONY: clean modules
+
+all: build/lib/libconv.so
+
+build/lib/libconv.so: modules conv/src/conv.cu
+	@mkdir -p build/.objects/conv
+	$(NVCC) -pg $(NVCCFLAGS) $(NVCCARCHS) -Xcompiler -fPIC \
+		-Iconv/include -I$(CUDAPATH)/samples/common/inc \
+		-dc -o build/.objects/conv/conv.o \
+		conv/src/conv.cu
+	$(NVCC) -pg $(NVCCFLAGS) $(NVCCARCHS) -Xcompiler -fPIC \
+		-dlink -o build/.objects/conv/conv-dlink.o build/.objects/conv/conv.o
+	mkdir -p build/lib
+	$(CC) -shared -o build/lib/libconv.so build/.objects/conv/* \
+		-Wl,-rpath=$(CUDAPATH)/lib64 -L$(CUDAPATH)/lib64 -lcudart
+	@mkdir -p build/include
+	@ln -sf ../../conv/include/conv.h build/include/conv.h
+	
--- conv/src/conv.cu
+++ conv/src/conv.cu
@@ -1,17 +1,19 @@
+#include <helper_cuda.h>
+
 __global__ void conv_kernel(float *N, float *F, float *P, int r, int height, int width) {
   int outCol=blockIdx.x*blockDim.x + threadIdx.x;
   int outRow=blockIdx.y*blockDim.y + threadIdx.y;
 
   float outPix = 0.0f;
-  for (frow=0; frow < 2*r+1; frow++) {
-    for (fcol=0; fcol<2*r+1; fcol++) {
-      inRow = outRow + frow - r;
-      inCol = outCol + fcol - r;
+  for (int frow=0; frow < 2*r+1; frow++) {
+    for (int fcol=0; fcol<2*r+1; fcol++) {
+      int inRow = outRow + frow - r;
+      int inCol = outCol + fcol - r;
       if (inRow >= 0 && inRow < height && inCol >= 0 && inCol < width)
         outPix += N[inRow*width + inCol] * F[frow*(r*2+1) + fcol];  
     }
   }
-  P[outRow*width + outCol];
+  P[outRow*width + outCol] = outPix;
 }
 
 void conv(float *N, float*F, float *P, int r, int height, int width) {
@@ -20,8 +22,8 @@ void conv(float *N, float*F, float *P, int r, int height, int width) {
   int gridSizeX = (width + blockSizeX -1) / blockSizeX;
   int gridSizeY = (height + blockSizeY -1) / blockSizeY;
 
-  dim3 blockSize = (blockSizeX, blockSizeY);
-  dim3 blockSize = (gridSizeX, gridSizeY);
+  dim3 blockSize(blockSizeX, blockSizeY);
+  dim3 gridSize(gridSizeX, gridSizeY);
 
   conv_kernel<<<gridSize, blockSize>>>(N, F, P, r, height, width);
 
