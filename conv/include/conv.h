5c7400b00c255c74a4e2c6ea8bf5b3d057e8a07b
james <jag0007@auburn.edu>
2023-02-25 14:10:44 -0600

    Add conv_shared_tile to conv library

--- conv/include/conv.h
+++ conv/include/conv.h
@@ -8,11 +8,13 @@ __constant__ float cF[25];
 enum class ConvType {
   CONV,
   CONVSHARED,
-  CONVCONST
+  CONVCONST,
+  CONVSHAREDTILE
 };
 
 void conv(unsigned char *N, float *F, unsigned char *P, int r, int height, int width);
 void conv_shared(unsigned char *N, float *F, unsigned char *P, int r, int height, int width);
 void conv_constant(unsigned char *N, float *F, unsigned char *P, int r, int height, int width);
+void conv_shared_tile(unsigned char *N, float *F, unsigned char *P, int r, int height, int width);
 
 #endif
--- conv/src/conv.cu
+++ conv/src/conv.cu
@@ -63,6 +63,51 @@ __global__ void conv_constant_kernel(unsigned char *N, float *F, unsigned char *
     P[outRow*width + outCol] = (unsigned char) outPix;
 }
 
+__global__ void conv_shared_tile_kernel(unsigned char *N, float *F, unsigned char *P, int r, int width, int height) {
+// assume tileDim == blockDim >= FilterDim  
+  extern __shared__ float s[];
+  float *F_ds = s;                                // shared filter data
+  unsigned char *N_ds = (unsigned char *)&F_ds[(2*r+1) * (2*r+1)];     // shared input data
+
+  // __shared__ F_ds[2*RADIUS+1][2*RADIUS+1];
+  // __shared__ N_ds[TILE_DIM][TILE_DIM];
+
+  int inputDim = blockDim.x;                      // input tile dim
+  int outputDim = inputDim - 2*r;                 // output tile dim
+
+  int inCol = blockIdx.x*outputDim + threadIdx.x - r;
+  int inRow = blockIdx.y*outputDim + threadIdx.y - r;
+
+  // load F into shared mem
+  if (threadIdx.x < 2*r + 1 && threadIdx.y < 2*r + 1) {
+    F_ds[threadIdx.y * 2*r+1 + threadIdx.x] = F[(threadIdx.y*2*r+1) + threadIdx.x];
+  }
+
+  // load input into shared
+  if (inRow >= 0 && inRow < height && inCol >= 0 && inCol < width) {
+    N_ds[threadIdx.y*width + threadIdx.x] = N[inRow*width + inCol];
+  } else {
+    N_ds[threadIdx.y*width + threadIdx.x] = 0.0f;
+  }
+  __syncthreads();
+
+  // do the conv
+  int tileCol = threadIdx.x - r;
+  int tileRow = threadIdx.y - r;
+  if (tileCol >= 0 && tileCol < outputDim && tileRow >= 0 && tileRow < outputDim) {
+    if (inRow >= 0  && inRow < height && inCol >= 0 && inCol < width) {
+      float outPix = 0.0f;
+      for (int frow = 0; frow < 2*r+1; ++frow) {
+        for (int fcol = 0; fcol < r*2 + 1; ++fcol) {
+          outPix += N[inRow*width + inCol] * F_ds[frow * 2*r+1 + fcol];
+        }
+      }
+      P[inRow * width + inCol] = outPix;  
+    }
+  }
+}
+
+
 void conv(unsigned char *N, float *F, unsigned char *P, int r, int height, int width) {
   int blockSizeX = 32;
   int blockSizeY = 32;
@@ -105,6 +150,23 @@ void conv_constant(unsigned char *N, float *F, unsigned char *P, int r, int heig
 
   int sharedMemorySize = (2*r+1) * (2*r+1) * sizeof(float);
   conv_constant_kernel<<<gridSize, blockSize, sharedMemorySize>>>(N, F, P, r, height, width);
+
+  checkCudaErrors(cudaGetLastError());
+  
+}
+
+void conv_shared_tile(unsigned char *N, float *F, unsigned char *P, int r, int height, int width) {
+  int blockSizeX = 32;
+  int blockSizeY = 32;
+  int gridSizeX = (width + blockSizeX -1) / blockSizeX;
+  int gridSizeY = (height + blockSizeY -1) / blockSizeY;
+
+  dim3 blockSize(blockSizeX, blockSizeY);
+  dim3 gridSize(gridSizeX, gridSizeY);
+
+  int sharedMemorySizeFilter = (2*r+1) * (2*r+1) * sizeof(float);
+  int sharedMemorySizeInputData = (width + 2*r) * (height + 2*r) * sizeof(unsigned char);
+  conv_constant_kernel<<<gridSize, blockSize, sharedMemorySizeFilter + sharedMemorySizeInputData>>>(N, F, P, r, height, width);
 
   checkCudaErrors(cudaGetLastError());
   
