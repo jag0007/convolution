12178ccc95bae8b665ada33f76933cfc3097add6
james <jag0007@auburn.edu>
2023-02-24 21:19:14 -0600

    Add conv_shared to conv.cu and conv.h

--- conv/include/conv.h
+++ conv/include/conv.h
@@ -2,9 +2,11 @@
 #define __conv_h__
 
 enum class ConvType {
-  CONV
+  CONV,
+  CONVSHARED
 };
 
 void conv(unsigned char *N, float *F, unsigned char *P, int r, int height, int width);
+void conv_shared(unsigned char *N, float *F, unsigned char *P, int r, int height, int width);
 
 #endif
