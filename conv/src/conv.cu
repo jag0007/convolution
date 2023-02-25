f8da867e0bc8e420304b10044165e5207b67bc33
james <jag0007@auburn.edu>
2023-02-24 21:26:10 -0600

    Fix compilation erros in conv.cu

--- conv/src/conv.cu
+++ conv/src/conv.cu
@@ -18,10 +18,10 @@ __global__ void conv_kernel(unsigned char *N, float *F, unsigned char *P, int r,
     P[outRow*width + outCol] = (unsigned char) outPix;
 }
 
-__global__ void conv_shared(unsigned char *N, float *F, unsigned char *P, int r, int width, int height) {
+__global__ void conv_shared_kernel(unsigned char *N, float *F, unsigned char *P, int r, int width, int height) {
 // assume tileDim == blockDim >= FilterDim  
   //__shared__ F_ds[2*RADIUS+1][2*RADIUS+1];
-  extern __shared__ float F_ds[]
+  extern __shared__ float F_ds[];
 
   int outCol = blockIdx.x*blockDim.x + threadIdx.x;
   int outRow = blockIdx.y*blockDim.y + threadIdx.y;
@@ -29,7 +29,7 @@ __global__ void conv_shared(unsigned char *N, float *F, unsigned char *P, int r,
 
 
   // load F into shared mem
-  if (threadIdx.x < 2*RADIUS + 1 && threadIdx.y < 2*RADUIS + 1) {
+  if (threadIdx.x < 2*r + 1 && threadIdx.y < 2*r + 1) {
     F_ds[threadIdx.y*(2*r+1) + threadIdx.x] = F[(threadIdx.y*(2*r+1)) + threadIdx.x];
   }
   __syncthreads();
@@ -37,8 +37,8 @@ __global__ void conv_shared(unsigned char *N, float *F, unsigned char *P, int r,
   // do the conv
   for (int frow = 0; frow < 2*r+1; ++frow) {
     for (int fcol = 0; fcol < r*2 + 1; ++fcol) {
-      inRow = frow + outRow - r;
-      inCol = fcol + outCol - r;
+      int inRow = frow + outRow - r;
+      int inCol = fcol + outCol - r;
       if (inRow >= 0 && inRow < height && inCol >= 0 and inCol < width) {
         outPix += ((float) N[inRow*width + inCol] * F_ds[frow * 2*r+1 + fcol]);
       }
@@ -73,7 +73,7 @@ void conv_shared(unsigned char *N, float *F, unsigned char *P, int r, int height
   dim3 blockSize(blockSizeX, blockSizeY);
   dim3 gridSize(gridSizeX, gridSizeY);
 
-  conv_kernel<<<gridSize, blockSize, (r*2+1) * (r*2+1) * sizeof(float)>>>(N, F, P, r, height, width);
+  conv_shared_kernel<<<gridSize, blockSize, (r*2+1) * (r*2+1) * sizeof(float)>>>(N, F, P, r, height, width);
 
   checkCudaErrors(cudaGetLastError());
   
